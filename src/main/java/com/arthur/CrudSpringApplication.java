a63c1dd3315596ff42779a50376acb04a24fd3b4
Arthur Bandeira <arthur.baandeira@gmail.com>
2024-02-24 15:18:09 -0300

    adicionando modificacoes

--- src/main/java/com/arthur/CrudSpringApplication.java
+++ src/main/java/com/arthur/CrudSpringApplication.java
@@ -1,7 +1,15 @@
 package com.arthur;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.arthur.model.Course;
+import com.arthur.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -11,21 +19,20 @@ public class CrudSpringApplication {
 	}
 
 //executa quando inicia a aplicação 
-//	@Bean
-//	CommandLineRunner initDataBase(CourseRepository courseRepository) {
-//		return args -> {
-//			courseRepository.deleteAll();
-//						
-//			List<Course> list = new ArrayList<>();
-//			list.add(new Course(null, "TESTE", "TESTE"));
-//			list.add(new Course(null, "TESTE", "TESTE"));
-//			list.add(new Course(null, "TESTE", "TESTE"));
-//			
-//			for (Course course : list) {
-//				courseRepository.save(course);				
-//			}
-//			
-//		};
-//	}
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+						
+			List<Course> list = new ArrayList<>();
+			list.add(new Course(null, "Java", "front-end"));
+			list.add(new Course(null, "Angular", "back-end"));
+			
+			for (Course course : list) {
+				courseRepository.save(course);				
+			}
+			
+		};
+	}
 
 }
--- src/main/java/com/arthur/controller/CoursesController.java
+++ src/main/java/com/arthur/controller/CoursesController.java
@@ -3,7 +3,14 @@ package com.arthur.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +20,42 @@ import com.arthur.repository.CourseRepository;
 @RestController
 @RequestMapping("/api/courses")
 public class CoursesController {
-	
+
 	@Autowired
 	private CourseRepository courseRepository;
-	
+
 	@GetMapping
 	public List<Course> list() {
 		return courseRepository.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Course> findById(@PathVariable Long id) {
+		return courseRepository.findById(id).map(c -> ResponseEntity.ok(c)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+//	@ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<Course> create(@RequestBody Course course) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+		return courseRepository.findById(id).map(c -> {
+			c.setName(course.getName());
+			c.setCategory(course.getCategory());
+			Course updated = courseRepository.save(c);
+			return ResponseEntity.ok(updated);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		return courseRepository.findById(id).map(c -> {
+			courseRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
 }
